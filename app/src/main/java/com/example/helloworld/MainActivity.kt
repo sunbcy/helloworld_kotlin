d4139899052b4039a08c39a4dc7ff82f78c21030
Bcy <626813534@qq.com>
2024-11-29 03:43:42 +0800

    更新Android kotlin 文件存储部分项目

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.helloworld
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +10,9 @@ import android.view.MenuItem
 
 import android.widget.Toast
 import com.example.helloworld.databinding.ActivityMainBinding
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.button1.setOnClickListener{
             Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
 
         binding.buttonFinish.setOnClickListener{
@@ -41,5 +48,24 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT).show()
         }
         return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val inputText = binding.editText.text.toString()
+        Log.d("data", inputText)
+        save(inputText)
+    }
+
+    private fun save(inputText: String){
+        try{
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            writer.use {
+                it.write(inputText)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.kt
+++ app/src/main/java/com/example/helloworld/SecondActivity.kt
@@ -1,4 +1,11 @@
 package com.example.helloworld
 
-class SecondActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_layout)
+    }
 }
